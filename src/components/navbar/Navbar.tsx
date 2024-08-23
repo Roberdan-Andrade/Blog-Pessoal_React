a8bac8a803c2880dc0ddeefb0200b1922d1b2444
Roberdan Andrade <Roberdan.andrade.dev@gmail.com>
2024-08-23 14:50:12 -0300

    Estilizando Navbar e Footer

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,22 @@
 function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+
+    return (
+        <>
+            <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+                <div className="container flex justify-between text-xl">
+                    <div className='text-3xl font-bold uppercase px-8'>Blog Pessoal</div>
+
+                    <div className='flex gap-4 px-4'>
+                        <div className='hover:underline'>Postagens</div>
+                        <div className='hover:underline'>Temas</div>
+                        <div className='hover:underline'>Cadastrar tema</div>
+                        <div className='hover:underline'>Perfil</div>
+                        <div className='hover:underline'>Sair</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,7 @@ function Home() {
         <div className="bg-indigo-900 flex justify-center"> {/*Inicio div 1*/}
 
             <div className="container grid grid-cols-2 text-white"> {/*Inicio div 2*/}
-                
+
                 <div className="flex flex-col gap-4 items-center justify-center py-4"> {/*Inicio div 3.1*/}
                     <h2 className="text-5xl font-bold">Seja Bem Vinde!</h2>
                     <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
@@ -16,7 +16,7 @@ function Home() {
                 </div> {/*Final div 3.1*/}
 
                 <div className="flex justify-center"> {/*Inicio div 3.2*/}
-                    <img className="w-2/3" src="https://i.imgur.com/VpwApCU.png" alt="Imagem pagina home"/>
+                    <img className="w-2/3" src="https://i.imgur.com/VpwApCU.png" alt="Imagem pagina home" />
                 </div>
             </div>
         </div>
